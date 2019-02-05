c5583b058c078ae18a91f3fcd52224a389cb0486
Chazzers <chazz567@gmail.com>
2019-02-05 10:55:54 +0100

    Opdrachten les 1

--- l1-fizzbuzz.js
+++ l1-fizzbuzz.js
@@ -1,1 +1,17 @@
-r
+let fizz = 'Fizz';
+let buzz = 'Buzz';
+
+for (var i = 1; i <= 100; i++) {
+  if(i % 3 == 0 && i % 5 == 0){
+    console.log(fizz+buzz);
+  }
+  else if(i % 5 == 0){
+    console.log(buzz);
+  }
+  else if(i % 3 == 0){
+    console.log(fizz);
+  }
+  else{
+    console.log(i);
+  }
+}
